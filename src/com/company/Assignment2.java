e2800ba3cf59a6068f089a5b816231046a6505d4
nicksantoscs <55286153+nicksantoscs@users.noreply.github.com>
2019-11-22 11:34:53 -0500

    Initial commit

---
+++ src/com/company/Assignment2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Assignment2 {
+
+public static void main (String[] args) {
+
+    WordPuzzle wordPuzzle = new WordPuzzle();
+    wordPuzzle.getWord();
+    }
+
+}
+
---
+++ src/com/company/WordPuzzle.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WordPuzzle {
+
+    private char[][] crossword;
+    private String wordlist;
+
+    public void getWord() {
+        int rows = getRows();
+        int columns = getColumns();
+    }
+
+    //Gets number of rows
+    public int getRows(String typeOfData) {
+        Scanner row = new Scanner(System.in);
+
+        int rowNum = 0;
+
+        do {
+            System.out.printf("Enter number of %s: ", typeOfData);
+            rowNum = row.nextInt();
+            if (rowNum < 2 || rowNum > 15) {
+                System.out.printf("Number of rows is not between 2 and 15, re-enter: ");
+            }
+        } while (rowNum < 2 || rowNum > 15);
+        return rowNum;
+    }
+
+    //Gets number of columns
+    public int getColumns() {
+        Scanner columns = new Scanner(System.in);
+
+        int colNum = 0;
+
+            do {
+            System.out.printf("Enter number of columns: ");
+            colNum = columns.nextInt();
+            if (colNum < 2 || colNum > 15) {
+                System.out.printf("Number of columns is not between 2 and 15, re-enter: ");
+            }
+        } while (colNum < 2 || colNum > 15);
+        return colNum;
+    }
+}
