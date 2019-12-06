604060bfaf6c4f49b8d5eb1cdab85cd1cb236575
Nicholas Santos <55286153+nicksantoscs@users.noreply.github.com>
2019-12-05 23:26:34 -0500

    Test upload to github

--- src/com/company/Assignment2.java
+++ src/com/company/Assignment2.java
@@ -1,15 +1,18 @@
+/**
+ * Nicholas Santos, December 5th 2019
+ */
 package com.company;
-
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Assignment2 {
 
 public static void main (String[] args) {
 
-    WordPuzzle wordPuzzle = new WordPuzzle();
-    wordPuzzle.getWord();
-    }
-
+    //Asks the user for all inputs
+    WordPuzzle puzzle = new WordPuzzle();
+    //Prints the grid for the puzzle based on numbers inputted by the user
+    System.out.println(puzzle.returnPuzzle());
+    //Prints the words to the puzzle depending on what the user inputs.
+    System.out.println(puzzle.wordList());
+}
 }
 
--- src/com/company/WordPuzzle.java
+++ src/com/company/WordPuzzle.java
@@ -1,47 +1,153 @@
+/**
+ * Nicholas Santos, December 5th 2019
+ */
 package com.company;
 
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class WordPuzzle {
 
     private char[][] crossword;
-    private String wordlist;
+    private String[] words;
+    private int rows = 0;
+    private int columns = 0;
 
-    public void getWord() {
-        int rows = getRows();
-        int columns = getColumns();
+    public WordPuzzle() {
+        rows = getRowsAndCols("rows");
+        columns = getRowsAndCols("columns");
+        crossword = new char[rows][columns];
+        words = new String[rows];
+        for (int i = 0; i < rows; i++) {
+            words[i] = getWord();
+        }
+        fillPuzzle();
+        returnWords();
     }
 
-    //Gets number of rows
-    public int getRows(String typeOfData) {
-        Scanner row = new Scanner(System.in);
+    /**
+     * This method will read the word input by the user and detect any possible errors.
+     *
+     * @return word that user inputs (and tells user if there is an error plus what the error is).
+     */
+    public String getWord() {
+        Scanner wordString = new Scanner(System.in);
+
+        String word;
+        do {
+            System.out.printf("Please input a word between 0 and %d letters: ", columns);
+            word = wordString.nextLine().trim().toUpperCase();
+
+            if (word.length() <= 1) {
+                System.out.printf("%s does not contain minimum number of letters, word must be greater than 1 characters%n", word);
+            }
+            /**
+             Statement handles the error for the user's word being too small.
+             */
+
+            if (word.contains(" ")) {
+                System.out.printf("Your word cannot contain a space%n");
+            }
+            /**
+             Statement handles the error for the user's word containing a space.
+             */
+
+            if (word.length() > columns) {
+                System.out.printf("%s exceeds the maximum number of letters, word must be <%d characters%n", word, columns);
+            }
+            /**
+             Statement handles the error for the user's word being too long.
+             */
+
+        } while (word.length() <= 1 || word.contains(" ") || word.length() > columns);
+        return word;
+    }
+
+    /**
+     * This method will fill the puzzle board with random letters based on an integer input by the user.
+     * Generates random number and adds 65 to get the ASCII code for the capital alphabet.
+     */
+
+    public void fillPuzzle() {
+        SecureRandom random = new SecureRandom();
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                crossword[y][x] = (char) (random.nextInt(25) + 65);
+            }
+        }
+    }
+
+    /**
+     * This method will prompt the user to enter a dimension and validate that it is between 2-15
+     */
+
+    public int getRowsAndCols(String typeOfData) {
+        Scanner impt = new Scanner(System.in);
+        String input = "";
+        int inputNum = 0;
 
         int rowNum = 0;
 
         do {
             System.out.printf("Enter number of %s: ", typeOfData);
-            rowNum = row.nextInt();
+            rowNum = impt.nextInt();
+
+            //Here I attempted to do a try catch so that the system would recognize if the user puts a letter/word in while it asks for the number of rows and columns.
+            //I couldn't understand why it wouldn't work after numerous attempts, I think it has to do with the SDK not recognizing input.parseInt. I decided to comment out the try catch section so that the rest of the program works
+            //If for some reason it IS my IntelliJ messing up then please remove the comments as a test.
+            //I didn't know how else to try it but I thought at least this way you'd see that I tried it. Thanks again.
+
+            /**try {
+                input = impt.next();
+                inputNum = input.parseInt(input);
+            }
+            catch (NumberFormatException e)‏{
+                System.out.println("Only integers are allowed");
+            }
+        */
             if (rowNum < 2 || rowNum > 15) {
                 System.out.printf("Number of rows is not between 2 and 15, re-enter: ");
             }
+
+
         } while (rowNum < 2 || rowNum > 15);
         return rowNum;
     }
 
-    //JW edit to remove
-    //Gets number of columns
-    public int getColumns() {
-        Scanner columns = new Scanner(System.in);
-
-        int colNum = 0;
-
-            do {
-            System.out.printf("Enter number of columns: ");
-            colNum = columns.nextInt();
-            if (colNum < 2 || colNum > 15) {
-                System.out.printf("Number of columns is not between 2 and 15, re-enter: ");
+    /**
+     * This method will return the number rows and columns that the user inputs
+     */
+    public String returnPuzzle() {
+        String wordGame = "";
+        for (int j = 0; j < rows; j++) {
+            for (int i = 0; i < columns; i++) {
+                wordGame += crossword[j][i] + "  ";
             }
-        } while (colNum < 2 || colNum > 15);
-        return colNum;
+            wordGame += "\n";
+        }
+        return wordGame;
+    }
+
+    /**
+     * This is the method that returns the words that the user inputs inside the puzzle
+     */
+    public void returnWords() {
+        int randomNum;
+        SecureRandom random = new SecureRandom();
+        for (int j = 0; j < rows; j++) {
+            randomNum = random.nextInt(columns - words[j].length() + 1);
+            for (int i = 0; i < columns; i++) {
+                if (i < (words[j].length() + randomNum) && i >= randomNum)
+                    crossword[j][i] = words[j].charAt(i - randomNum);
+            }
+        }
+    }
+    /** This is the method that lists the words that the user inputs after the puzzle is made*/
+    public String wordList()   {
+        String listOfWords = "";
+        for (int i = 0; i < rows; i++) {
+            listOfWords += words[i] + "\n";
+        }
+        return listOfWords;
     }
 }
